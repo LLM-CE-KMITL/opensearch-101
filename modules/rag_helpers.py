aa673769a14e17d5347951aeebfbc272cb519c79
Rathachai CHAWUTHAI <rathachai.c@gmail.com>
2025-05-07 14:21:05 +0700

    Update rag_helpers.py

--- modules/rag_helpers.py
+++ modules/rag_helpers.py
@@ -87,7 +87,7 @@ class RAG_OpenSearch:
                             "dimension": 1024,  #size of BGE
                             "method": {
                                 "name": "hnsw",
-                                "engine": "nmslib",
+                                "engine": "faiss", # nmslib
                                 "space_type": "cosinesimil"
                             }
                         }
